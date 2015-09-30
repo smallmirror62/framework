59100a7bbab92a6e4a7e15dc2043c5af72de45a5
XuTongle <xutongle@gmail.com>
2015-09-30 17:16:46 +0800

    Application

---
+++ src/Leaps/Application/ConsoleApplication.php
@@ -0,0 +1,45 @@
+<?php
+// +----------------------------------------------------------------------
+// | Leaps Framework [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011-2014 Leaps Team (http://www.tintsoft.com)
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author XuTongle <xutongle@gmail.com>
+// +----------------------------------------------------------------------
+namespace Leaps\Application;
+
+class ConsoleApplication extends \Leaps\Core\Application
+{
+
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @param resource Leaps\Http\Request
+	 * @see \Leaps\Core\Application::handleRequest()
+	 */
+	public function handleRequest($request)
+	{
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see \Leaps\Core\Application::coreServices()
+	 */
+	public function coreServices()
+	{
+		return [
+				"cookie" => [
+						"className" => "\\Leaps\\Http\\Cookies"
+				],
+				"request" => [
+						"className" => "\\Leaps\\Http\\Request"
+				],
+				"response" => [
+						"className" => "\\Leaps\\Http\\Response"
+				]
+		];
+	}
+}
