896b275f5daa9d7fb805f3f98209326d0a66554e
XuTongle <xutongle@gmail.com>
2015-10-10 14:43:01 +0800

    ErrorHandler update

--- src/Leaps/Core/Controller.php
+++ src/Leaps/Core/Controller.php
@@ -9,9 +9,35 @@
 // | Author XuTongle <xutongle@gmail.com>
 // +----------------------------------------------------------------------
 namespace Leaps\Core;
-class Controller{
+class Controller {
 
-	public function __construct(){
+	/**
+	 * 控制器ID
+	 * @var string
+	 */
+	public $id;
 
-	}
+	/**
+	 * 该控制器所属模块
+	 * @var Module $module
+	 */
+	public $module;
+
+	/**
+     * @param string $id 控制器ID
+     * @param Module $module 该控制器所属模块
+     * @param array $config 用来初始化对象属性的数组
+     */
+    public function __construct($id, $module, $config = [])
+    {
+        $this->id = $id;
+        $this->module = $module;
+        //parent::__construct($config);
+    }
+
+    /**
+     * 默认操作
+     * @var string
+     */
+    public $defaultAction = 'index';
 }
